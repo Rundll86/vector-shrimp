26251780c1967948eaa1579b6c0f346f86ed0e0c
陨落基围虾 <3161880837@qq.com>
2025-12-26 22:35:51 +0800

    feat: 初始化项目基础结构
    添加项目基础文件结构，包括Vue组件、TypeScript配置、Webpack配置、ESLint配置和package.json依赖

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+import VectorShrimp from "./components/VectorShrimp.vue";
+</script>
+<template>
+    <VectorShrimp />
+</template>
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+const Webpackbar = require("webpackbar");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+/**
+ * @type {import('webpack').Configuration}
+ */
+module.exports = {
+    entry: "./src/index.ts",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "app.js",
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: "vue-loader",
+            },
+            {
+                test: /\.css$/,
+                use: ["vue-style-loader", "css-loader"],
+            },
+            {
+                test: /\.ts$/,
+                use: {
+                    loader: "ts-loader",
+                    options: {
+                        appendTsSuffixTo: [/\.vue$/],
+                        transpileOnly: true,
+                    },
+                },
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new Webpackbar({
+            name: "VectorShrimp",
+            color: "green",
+        }),
+        new HtmlWebpackPlugin({
+            template: "templates/index.html",
+            filename: "index.html",
+        }),
+    ],
+    stats: "errors-warnings",
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, "public"),
+        },
+        compress: true,
+        port: 25565,
+        setupExitSignals: false,
+        client: {
+            logging: "none"
+        },
+        open: true
+    }
+};
